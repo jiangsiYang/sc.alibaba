6ed54fef12054831bd4954bb4c5d2d48b2e5a6f6
æ杨进 <yangjiangsi@qunjielong.com>
2019-11-16 16:20:42 +0800

    nacos config 模块

---
+++ nacos-config/src/main/java/com/sc/alibaba/nacos/AlibabaNacosDiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package com.sc.alibaba.nacos;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaNacosDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaNacosDiscoveryServerApplication.class, args);
+    }
+
+}
