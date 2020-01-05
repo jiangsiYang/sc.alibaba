cc383f3354c36a8330e83c9d9c37c0b408e952bd
jiangsiYang <353098889@qq.com>
2020-01-05 10:45:08 +0800

    完成 nacos和dubbo的结合 demo

--- nacos-dubbo/nacos-dubbo-client/src/main/java/com/dubbo/DubboClientApplication.java
+++ nacos-dubbo/nacos-dubbo-client/src/main/java/com/dubbo/DubboClientApplication.java
@@ -1,6 +1,34 @@
 package com.dubbo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class DubboClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboClientApplication.class, args);
+    }
+
+    @Slf4j
+    @RestController
+    static class TestController {
+
+        /**
+         * 注意,这里的Reference是org.apache.dubbo.config.annotation.Reference
+         * 然后很神奇的就能直接引用server模块的service了
+         */
+        @Reference
+        HelloService helloService;
+
+        @GetMapping("/test")
+        public String test() {
+            return helloService.hello("didispace.com");
+        }
+    }
 }
--- nacos-dubbo/nacos-dubbo-server/src/main/java/com/dubbo/HelloServiceImpl.java
+++ nacos-dubbo/nacos-dubbo-server/src/main/java/com/dubbo/HelloServiceImpl.java
@@ -1,5 +1,12 @@
 package com.dubbo;
 
+import org.apache.dubbo.config.annotation.Service;
+
+/**
+ * 注意：这里的@Service注解不是Spring的，而是org.apache.dubbo.config.annotation.Service注解
+ * 然后client模块就能通过@Reference注解引用到这里了
+ */
+@Service
 public class HelloServiceImpl implements HelloService {
     @Override
     public String hello(String name) {
