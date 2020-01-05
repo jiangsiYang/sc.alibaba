3229d1ff0e1dd1f21fa8bc3d43a2019afadc7411
jiangsiYang <353098889@qq.com>
2020-01-05 22:32:14 +0800

    增加了ä¸个非主注解的demo

--- sentinel-rate-limiting/src/main/java/com/alibaba/sentinel/limiting/service/TestService.java
+++ sentinel-rate-limiting/src/main/java/com/alibaba/sentinel/limiting/service/TestService.java
@@ -1,8 +1,16 @@
 package com.alibaba.sentinel.limiting.service;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TestService {
@@ -13,5 +21,51 @@ public class TestService {
 
     public void exceptionHandler(String str, BlockException ex) {
         System.out.println("blockHandler：" + str + "ex=" + ex);
+    }
+
+
+    public static void main(String argsp[]){
+        testOriginCode();
+    }
+
+    /**
+     * 把需要控制流量的代码用 Sentinel API SphU.entry("HelloWorld") 和 entry.exit() 包围起来即可。在下面的例子中，
+     * 我们将 System.out.println("hello wolrd"); 作为资源，用 API 包围起来。
+     */
+    public static void testOriginCode() {
+        initFlowRules();
+        while (true) {
+            Entry entry = null;
+            try {
+                entry = SphU.entry("HelloWorld");
+                //业务逻辑开始
+                System.out.println("hello world");
+                //业务逻辑结束
+            } catch (BlockException e) {
+                //流控逻辑处理
+                System.out.println("block!");
+                //流控逻辑结束
+            } finally {
+                if (entry != null) {
+                    entry.exit();
+                }
+            }
+
+
+        }
+    }
+
+    /**
+     * 通过规则来指定允许该资源通过的请求次数，例如下面的代码定义了资源 HelloWorld 每秒最多只能通过 20 个请求。
+     */
+    private static void initFlowRules() {
+        List<FlowRule> ruleList = new ArrayList<>();
+        FlowRule flowRule = new FlowRule();
+        flowRule.setResource("HelloWorld");
+        flowRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // set limit QPS to 20
+        flowRule.setCount(20);
+        ruleList.add(flowRule);
+        FlowRuleManager.loadRules(ruleList);
     }
 }
